5c08e2691d298ac7f8a19a34ad94da38d6ce8318
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-19 15:37:46 +0200

    fine esercizio

---
+++ src/components/ComponentMain/FumettiComp.vue
@@ -0,0 +1,47 @@
+<template>
+  
+      
+      
+         
+            <div>
+              <img :src="img" alt="">
+              <h3>{{titolo}}</h3>
+            </div>
+          
+       
+
+
+    
+  
+</template>
+
+<script>
+export default {
+  name: 'FumettiComp',
+    props:{
+        img:String,
+        titolo:String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+   
+
+            
+
+                div{
+                    margin: 5px 30px;
+                    flex-basis: calc((100%/6) - 60px);
+                    img{
+                        width: 100%;
+                    }
+                    h3{
+                        color: white;
+                    }
+                }
+            
+        
+    
+</style>
---
+++ src/components/ComponentMain/HeroComp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <div class="hero">
+        <button>
+          current series
+        </button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroComp',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../../assets/style/variabili.scss';
+.hero{
+    background-image: url("../../assets/img/jumbotron.jpg");
+    height: 400px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: relative;
+   button{
+      font-size: 1.2em;
+      text-transform: uppercase;
+      padding: 5px 10px;
+      background: $blu;
+      color: $bianco;
+      border: 0;
+      position: absolute;
+      bottom: -20px;
+      left: 11%;
+   }
+}
+</style>
